5d38e6d54e0a8f1bbcff635b8d98ab40d64e7fb7
Hiba Almanakly <eng.hibaalmanakly@gmail.com>
2021-02-28 21:53:22 +0200

    Update App component & module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UsersModule} from './modules/users/users.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  {
+    path: '', component: AppComponent
+  },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    UsersModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
